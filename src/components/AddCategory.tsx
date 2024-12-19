67ed3a8953543abbe09a96817186e5560760a7be
Jesus Zeballos <iijandgii@gmail.com>
2024-12-19 13:17:37 -0400

    Add AddCategory.tsx to enforce  Single Responsability Principle

--- src/components/AddCategory.tsx
+++ src/components/AddCategory.tsx
@@ -1,11 +1,47 @@
+import { FormEvent, useState } from "react"
+import { Category } from "../../interfaces/CategoryInterface";
 
-export const AddCategory : React.FC = () => {
+export const AddCategory : React.FC<Category> = ({ categories, setCategories }) => {
+
+  const [initialValue, setInitialValue] = useState("");
+  const [errorMessage, setErrorMessage] = useState("");
+
+  const onSubmit = ( event : FormEvent) : void  => {
+    event.preventDefault();
+    if( initialValue.trim().length === 0 ) {
+      setErrorMessage('The field is empty');
+      return;
+    }
+
+    if( categories?.includes(initialValue) ) {
+      setErrorMessage('La categoria ya existe');
+      setInitialValue('');
+      return;
+    }
+    setCategories( (category : Array<string>) => [...category, initialValue.toLocaleLowerCase()]);
+
+    setInitialValue('');
+    setErrorMessage('');
+
+  }
+  
+  const onInputChange = ( { target } : FormEvent) : void  => {
+    const { value } = target as HTMLInputElement;
+    setInitialValue( value );
+  }
+  
+  
   return (
-    <form >
+    <form onSubmit={ onSubmit }>
       <input 
        type="text" 
         placeholder="Buscar Gif"
+        value={ initialValue }
+        onChange={ onInputChange }
       />
+      {
+        errorMessage && <p>{ errorMessage }</p>
+      }
     </form>
   )
 }
